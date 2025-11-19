0f0ca6b6a37cc707e16338e8e1192f4d36d68690
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-11-19 11:25:13 +0530

    fixed

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -8,7 +8,6 @@ router.get('/get-projects', auth, listProjects);
 router.post('/create-project', auth, createProject);
 router.patch('/update-project/:id', auth, updateProject);
 router.delete('/delete-project/:id', auth, deleteProject);
-// router.post("/:projectId/invite", auth, inviteMember);
 router.get("/:projectId/project-details", auth, getProjectDetails);
 router.get("/:id/invite/search", auth, searchUsersToInvite);
 router.delete("/:id/remove-member/:memberId", auth, removeProjectMember);
